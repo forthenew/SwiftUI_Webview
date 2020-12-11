7adbda720b545cf7613fc168eada1375830ec971
forthenew <forthenew@gmail.com>
2020-12-11 14:08:04 +0900

    Initial Commit

---
+++ SwiftUI_Webview/SwiftUI_WebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WebviewApp.swift
+//  SwiftUI_Webview
+//
+//  Created by stam on 2020/12/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
