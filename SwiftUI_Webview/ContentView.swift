2ad2f1d8786e25a6af355fad82831550a7bdb563
forthenew <forthenew@gmail.com>
2020-12-11 14:54:23 +0900

    최초 설정

--- SwiftUI_Webview/ContentView.swift
+++ SwiftUI_Webview/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            HStack{
+                NavigationLink(
+                    destination: MyWebview(urlToLoad: "https://www.navienhouse.com").ignoresSafeArea(),
+                    label: {
+                        Text("나비엔하우스")
+                            .font(.system(size: 20))
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .padding(20)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            .background(Color.red)
+                            .cornerRadius(5)
+                    })
+                NavigationLink(
+                    destination: MyWebview(urlToLoad: "http://www.naver.com").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/),
+                    label: {
+                        Text("네이버")
+                            .font(.system(size: 20))
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .padding(20)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            .background(Color.green)
+                            .cornerRadius(5)
+                    })
+            }
+        }
     }
 }
 
